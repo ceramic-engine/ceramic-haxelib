437603fa1757bfaa77396b7c05fc6f1903936f0a
Jeremy Faivre <contact@jeremyfa.com>
2019-11-25 18:04:05 +0100

    Set things up

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ceramic-haxelib
+
 Install ceramic via haxelib
+
+Note: this library does nothing at the moment. Just setting things up and reserving the name on haxelib repo!
---
+++ src/Main.hx
@@ -0,0 +1,12 @@
+package;
+
+class Main {
+
+    public static function main():Void {
+
+        Sys.println('Hello! This library does nothing for now.');
+        Sys.println('Next versions will allow to install ceramic engine via haxelib!');
+
+    }
+
+} //Main
