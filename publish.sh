dcad03d9cab691720ab80ef78bb01da7cb5365a1
Jeremy Faivre <contact@jeremyfa.com>
2020-12-19 22:21:49 +0100

    Prepare 0.2.0 release

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ cd "${0%/*}"
 rm -rf ./ceramic
 rm -rf ./ceramic.zip
 rm -rf ./ceramic-linux.zip
-rm -rf ./ceramic-windows.zip
+rm -rf ./ceramic-mac.zip
 rm -rf ./ceramic-windows.zip
 
 haxelib submit .
